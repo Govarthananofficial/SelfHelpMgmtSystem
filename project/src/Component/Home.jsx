a4c3c771d731c6dbac836605173b69570aa0109b
rajeammu <raje30pa@gmail.com>
2023-08-17 16:27:48 +0530

    changes in home

--- project/src/Component/Home.jsx
+++ project/src/Component/Home.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
-
+import './Home.css';
+import { FaUser } from 'react-icons/fa';
+import { FaCommentDots } from 'react-icons/fa';
+import { FaComments } from 'react-icons/fa';
+import { FaUsers } from 'react-icons/fa'
+import { BsFillCalendarEventFill } from 'react-icons/bs';
+import { HiCurrencyRupee } from 'react-icons/hi';
+import { FaUserPlus } from 'react-icons/fa';
 function Home() {
   
     return (
-      <div>
-        
+      <div class="home">
+      
+        <div id="home1"><span id="user1"><FaUser/></span>Users</div>
+        <div  id="home2"><span id="user1"><FaCommentDots/></span>Complaints</div>
+        <div id="home3"><span id="user1"><FaComments/></span>Comments</div>
+        <div id="home4"><span id="user1"><FaUsers/></span><br></br>Staff</div>
+        <div id="home5"><span id="user1"><BsFillCalendarEventFill/></span><br></br>Events</div>
+        <div id="home6"><span id="user1"><HiCurrencyRupee/></span><br></br>Loan</div>
+        <div id="home7"><span id="user1"><FaUserPlus/></span><br></br>Member</div>
       </div>
     )
   
