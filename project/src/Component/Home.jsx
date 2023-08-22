9d70e10fa773d566f92f96678a4654007c05dfe9
rajeammu <raje30pa@gmail.com>
2023-08-22 16:26:53 +0530

    chnged

--- project/src/Component/Home.jsx
+++ project/src/Component/Home.jsx
@@ -1,27 +1,32 @@
-import React from 'react'
-import './Home.css';
-import { FaUser } from 'react-icons/fa';
-import { FaCommentDots } from 'react-icons/fa';
-import { FaComments } from 'react-icons/fa';
-import { FaUsers } from 'react-icons/fa'
-import { BsFillCalendarEventFill } from 'react-icons/bs';
-import { HiCurrencyRupee } from 'react-icons/hi';
-import { FaUserPlus } from 'react-icons/fa';
-function Home() {
-  
-    return (
-      <div class="home">
-      
-        <div id="home1"><span id="user1"><FaUser/></span>Users</div>
-        <div  id="home2"><span id="user1"><FaCommentDots/></span>Complaints</div>
-        <div id="home3"><span id="user1"><FaComments/></span>Comments</div>
-        <div id="home4"><span id="user1"><FaUsers/></span><br></br>Staff</div>
-        <div id="home5"><span id="user1"><BsFillCalendarEventFill/></span><br></br>Events</div>
-        <div id="home6"><span id="user1"><HiCurrencyRupee/></span><br></br>Loan</div>
-        <div id="home7"><span id="user1"><FaUserPlus/></span><br></br>Member</div>
-      </div>
-    )
-  
-}
+import React from 'react';
+import {
+  MDBCarousel,
+  MDBCarouselItem,
+} from 'mdb-react-ui-kit';
 
-export default Home;
+export default function Home() {
+  return (
+    <div class="camera">
+    <MDBCarousel showControls touch={false}>
+      <MDBCarouselItem
+        className='w-50 d-block'
+        itemId={1}
+        src='https://mdbootstrap.com/img/new/slides/041.jpg'
+        alt='...'
+      />
+      <MDBCarouselItem
+        className='w-50 d-block'
+        itemId={2}
+        src='https://mdbootstrap.com/img/new/slides/042.jpg'
+        alt='...'
+      />
+      <MDBCarouselItem
+        className='w-50 d-block'
+        itemId={3}
+        src='https://mdbootstrap.com/img/new/slides/043.jpg'
+        alt='...'
+      />
+    </MDBCarousel>
+    </div>
+  );
+}
